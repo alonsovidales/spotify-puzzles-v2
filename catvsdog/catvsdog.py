65c57d8ea22b39b8b57371eb806530817e0752af
Alonso Vidales <alonso.vidales@tras2.es>
2013-09-08 19:21:20 +0200

    Modified according to PEP 8

--- catvsdog/catvsdog.py
+++ catvsdog/catvsdog.py
@@ -8,71 +8,71 @@ from bipartite_match import bipartiteMatch
 
 class CatVsDog:
     __debug = False
-    __votesList = None
+    __votes_list = None
 
     def resolve(self):
         # This graph will contain all the conflictive relations between voters
-        bipartiteByCat = {}
+        bipartite_by_cat = {}
 
-        catLovers = []
+        cat_lovers = []
         # Dog lovers groupped by the dog who they loves
-        byDogLove = {}
+        by_dog_love = {}
         # Dog lovers groupped by the cat who they hate
-        byDogHate = {}
+        by_dog_hate = {}
 
         # This counter will be used to avoid problems with duplicate votes on the
         # sets and the key of the dict who conforms the graph
         count = 0
-        for vote in self.__votesList:
+        for vote in self.__votes_list:
             vote = "%s %d" % (vote, count)
             count += 1
             if vote[0] == 'D':
-                voteParts = vote.split()
+                vote_parts = vote.split()
 
-                if voteParts[0] in byDogLove:
-                    byDogLove[voteParts[0]].add(vote)
+                if vote_parts[0] in by_dog_love:
+                    by_dog_love[vote_parts[0]].add(vote)
                 else:
-                    byDogLove[voteParts[0]] = set([vote])
+                    by_dog_love[vote_parts[0]] = set([vote])
 
-                if voteParts[1] in byDogHate:
-                    byDogHate[voteParts[1]].add(vote)
+                if vote_parts[1] in by_dog_hate:
+                    by_dog_hate[vote_parts[1]].add(vote)
                 else:
-                    byDogHate[voteParts[1]] = set([vote])
+                    by_dog_hate[vote_parts[1]] = set([vote])
             else:
-                catLovers.append(vote)
+                cat_lovers.append(vote)
 
         # Create the bipartite graph using a dictionary, the key will be the vote of the cat lover, and the
         # values a set with all the votes who have problems with the vote of the cat lover at the key
-        for catVote in catLovers:
-            catVoteParts = catVote.split()
-            bipartiteByCat[catVote] = byDogLove.get(catVoteParts[1], set()) | byDogHate.get(catVoteParts[0], set())
+        for cat_vote in cat_lovers:
+            cat_vote_parts = cat_vote.split()
+            bipartite_by_cat[cat_vote] = by_dog_love.get(cat_vote_parts[1], set()) | by_dog_hate.get(cat_vote_parts[0], set())
 
         # Use the Hopcroft-Karp algorith in order to determinate the max cardinality, then we will know the
         # min number of conflictive votters that we have
-        maxMatching, pred, unlayered = bipartiteMatch(bipartiteByCat)
+        max_matching, pred, unlayered = bipartiteMatch(bipartite_by_cat)
 
         if self.__debug:
-            print "Votes: %s" % (bipartiteByCat)
-            print "Max matching %s" % (maxMatching)
-            print "Dog love: %s" % (byDogLove)
-            print "Dog hate: %s" % (byDogHate)
+            print "Votes: %s" % (bipartite_by_cat)
+            print "Max matching %s" % (max_matching)
+            print "Dog love: %s" % (by_dog_love)
+            print "Dog hate: %s" % (by_dog_hate)
 
 
         # The max number of happy voters are the number of total voters minus the number of conflictive pairs (we can remove
         # one of the members of each pair)
-        return len(self.__votesList) - len(maxMatching)
+        return len(self.__votes_list) - len(max_matching)
 
-    def __init__(self, inVotes):
-        self.__votesList = inVotes
+    def __init__(self, votes):
+        self.__votes_list = votes
 
 if __name__ == "__main__":
     # I'll use raw_input to get the lines because I can't import fileinput on the test server
     problems = int(raw_input())
     results = []
     for problem in xrange(0, problems):
-        problemInfo = map(int, raw_input().split())
+        problem_info = map(int, raw_input().split())
         votes = []
-        for problemLine in xrange(0, problemInfo[2]):
+        for problem_line in xrange(0, problem_info[2]):
             votes.append(raw_input())
 
         results.append(CatVsDog(votes).resolve())
